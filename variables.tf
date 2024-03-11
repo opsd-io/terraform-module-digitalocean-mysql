68be7e772417c50073e2ce612ff137a3ad5918d3
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-11 13:31:19 +0100

    First release ready to test

--- variables.tf
+++ variables.tf
@@ -1,12 +1,37 @@
-# variable "variable_name" {
-#	  description = "variable description"
-#   type        = number
-#   default     = 1
-# }
+variable "databasename" {
+  description = "The name of the database cluster. Required"
+  type        = string
+  nullable    = false
+}
+variable "mysql_main_node_count" {
+  description = "Number of mysql nodes that will be created. Required"
+  type        = number
+  default     = 1
+}
 
-# variable "variable_password" {
-#	  description = "variable description"
-#   type        = string
-#   sensitive   = true
-#   default     = "abc"
-# }
+variable "engine" {
+  description = "Database engine used by the cluster. Required"
+  type        = string
+  default     = "mysql"
+}
+
+variable "mysql_main_size" {
+  description = "The mysql node instance size. Required"
+  type        = string
+  default     = "db-s-1vcpu-1gb"
+}
+variable "version_of_engine" {
+  description = "Engine version used by the cluster. Required"
+  type        = number
+  default     = 8
+}
+
+variable "region" {
+  description = "DigitalOcean region where the cluster will reside. Required"
+  type        = string
+  nullable    = false
+}
+variable "common_tags" {
+  description = "A list of tag names to be applied to the database cluster"
+  type        = set(string)
+}
